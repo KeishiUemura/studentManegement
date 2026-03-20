d3a9d82de02fa6f1dc3e8e8a9dd2dd3f4100e662
Keishi Uemura <L2505922257@gmail.com>
2026-03-20 17:50:17 +0900

    課題前コミット

--- src/main/java/StudentManegementSystem/StudentManegementApplication.java
+++ src/main/java/StudentManegementSystem/StudentManegementApplication.java
@@ -1,20 +1,40 @@
 package StudentManegementSystem;
 
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManegementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManegementApplication.class, args);
-	}
+  private String name = "Uemura Keishi";
+  private String age = "30";
 
-	@GetMapping("/spring")
-	public String spring() {
-		return "Javaエンジニアスタンダード 第2回演習課題";
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManegementApplication.class, args);
+  }
+
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
+  }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
+  }
+  // GET POST
+  // GETは取得する、リクエストの結果を受け取る
+  // POSTは情報を与える、渡す
 }
